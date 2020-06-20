e9b028ccfd454a3ed4304223b5c9f8fb9aad194c
Ben Timor <me@bentimor.com>
2020-06-20 19:12:02 +0300

    fixed __init__

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ import sys
 import StrawEngine
 sys.path.append(StrawEngine.__file__[::-1].split("/",1)[1][::-1])
 from StrawEngine.straw import main as mainapi
-main = lambda x: mainapi(x, True)
+main = lambda: mainapi(sys.argv, True)
